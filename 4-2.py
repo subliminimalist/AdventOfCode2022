3baf876b7ab1e1eff350a49ed31b97e2df9c3f5e
subliminimalist <benrhaas@gmail.com>
2022-12-06 20:53:44 -0500

    Day 4 complete

---
+++ 4-2.py
@@ -0,0 +1,22 @@
+file = open(r'.\inputs\4-1.txt')
+
+def is_total_overlap(assignment_1, assignment_2):
+    if int(assignment_1.split('-')[0]) >= int(assignment_2.split('-')[0]) and int(assignment_1.split('-')[1]) <= int(assignment_2.split('-')[1]):
+        return True
+    else:
+        return False
+
+def is_partial_overlap(assignment_1, assignment_2):
+    if (int(assignment_1.split('-')[0]) >= int(assignment_2.split('-')[0]) and int(assignment_1.split('-')[0]) <= int(assignment_2.split('-')[1])) or (int(assignment_1.split('-')[1]) >= int(assignment_2.split('-')[0]) and int(assignment_1.split('-')[1]) <= int(assignment_2.split('-')[1])):
+        return True
+    else:
+        return False
+
+overlap_count = 0
+
+for line in file:
+    assignments = line.replace('\n','').split(',')
+    if is_partial_overlap(assignments[0], assignments[1]) or is_partial_overlap(assignments[1], assignments[0]):
+        overlap_count += 1
+
+print(overlap_count)
